b8dc7930414388cdb0bf54ff03fb883262b2dc0f
Teste <otavianomattos10@gmail.com>
2021-12-10 22:52:58 -0400

    Finalização dos ajustes para geração do aplicativo React + Electron + Typescript.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
 import { Component } from 'react';
 
-const electron = window.require("electron");
-const ipcRenderer = electron.ipcRenderer;
-
 class App extends Component {
 
   state = {
@@ -13,9 +10,7 @@ class App extends Component {
   }
 
   async componentDidMount(){
-    
-    //const response = await axios.get("http://localhost:3000/api/realmdb");
-    
+        
     this.setState({tasks: this.state.tasks});
 
   }
@@ -23,16 +18,16 @@ class App extends Component {
   render(){        
     return(
       <div>
-        <h1>Lista de Tarefas</h1>        
-        
+        <h1>Prioridades VininhU!</h1>                
           <h2>
             <div>
-            <h2>ID: </h2>
-            000001
-            </div>       
+            <h2>Quem meu VinU sempre prefere?</h2>            
+            </div>                     
             <div>
-            <h2>Nome: </h2>
-            VinU
+              ZizU
+            </div>       
+            <div>        
+              Blyto      
             </div>     
           </h2>                
       </div>
