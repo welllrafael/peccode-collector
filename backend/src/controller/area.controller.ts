c4515c048bf0668f113984cef53bab12c8f39fe0
Teste <otavianomattos10@gmail.com>
2021-12-17 16:44:18 -0400

    - Ajuste na nomenclatura do controller - Configuração do module da area

--- backend/src/controller/area.controller.ts
+++ backend/src/controller/area.controller.ts
@@ -2,26 +2,26 @@ import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { AreaService } from '../service/area.service';
 
 @Controller("/api/area")
-export class AppController {
+export class AreaController {
   constructor(private readonly appService: AreaService) {}
 
   @Get()
-  async getAllTasks(): Promise<void> {
+  async getAllArea(): Promise<void> {
     return await this.appService.getArea();
   }
 
   @Post()
-  async sendAllTasks(): Promise<void> {
+  async postAllArea(): Promise<void> {
     return await this.appService.postArea();
   }
 
   @Put()
-  async updateAllTasks(): Promise<void> {
+  async putAllArea(): Promise<void> {
     return await this.appService.putArea();
   }
 
   @Delete()
-  async deleteAllTasks(): Promise<void> {
+  async deleteAllArea(): Promise<void> {
     return await this.appService.deleteArea();
   }
 }
--- backend/src/module/area.module.ts
+++ backend/src/module/area.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AreaController } from '../controller/area.controller';
 import { AreaService } from '../service/area.service';
 
 @Module({
-  providers: [AreaService]
+  providers: [AreaService],
+  controllers: [AreaController]
 })
 export class AreaModule {}
